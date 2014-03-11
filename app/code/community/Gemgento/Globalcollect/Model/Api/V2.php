e2d00b906b796c7e1264db3badd8109e4cf5d136
Kevin <kevin.pheasey@gmail.com>
2014-03-11 16:06:07 -0400

    Add Gemgento copyright notice

--- app/code/community/Gemgento/Globalcollect/Model/Api/V2.php
+++ app/code/community/Gemgento/Globalcollect/Model/Api/V2.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @package     Gemgento_Globalcollect
+ * @copyright   Copyright © 2014 Gemgento LLC
+ */
 
 class Gemgento_Globalcollect_Model_Api_V2 extends Gemgento_Globalcollect_Model_Api {
     //empty
